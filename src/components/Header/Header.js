5baa81164f19fb56d5cb6f7137edcb3e68307d9d
Jessica <jdhpabst@gmail.com>
2017-08-08 14:25:37 -0600

    working on ham menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,12 +9,12 @@ class Header extends Component {
     return (
       <div className="header">
           <section id="mobile_header">
-              <div className = 'ham'>
+              <div className='ham'>
                   <div id="bar1"></div>
                   <div id="bar2"></div>
                   <div id="bar3"></div>
               </div>
-              <h1 className = "header_logo">Artsy</h1>
+              <h1 className="header_logo">Artsy</h1>
           </section>
 
       </div>
