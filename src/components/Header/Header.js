9ff56dd8e3689b7586acbb5ee598663347dd9cbf
Jessica <jdhpabst@gmail.com>
2017-08-08 15:57:09 -0600

    finished with mobile header (except for links)

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -30,17 +30,17 @@ class Header extends Component {
     return (
       <div className="header">
           <section id="mobile_header">
-              <div className='ham'  onClick={ this.handleDropDown }>
+                <div className='ham'  onClick={ this.handleDropDown }>
                   <div id="bar1"></div>
                   <div id="bar2"></div>
                   <div id="bar3"></div>
-              </div>
-            <section id="mobile_menu">
-                <div>HOME</div>
-                <div>SHOP</div>
-                <div>CART</div>
-            </section>
-              <h1 className="header_logo">Artsy</h1>
+                </div>
+                <section id="mobile_menu">
+                    <div>HOME</div>
+                    <div>SHOP</div>
+                    <div>CART</div>
+                </section>
+                <h1 className="header_logo">Artsy</h1>
           </section>
 
       </div>
