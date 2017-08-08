c5974777ca39ef08f536e8ac53dfb2ddc4652297
Jessica <jdhpabst@gmail.com>
2017-08-08 14:20:10 -0600

    working on header content

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,11 @@ class Header extends Component {
   render() {
     return (
       <div className="header">
-          Header Area
+          <section id="mobile_header">
+              <div className = 'ham'><img src=''/></div>
+              <h1 className = "header_logo">Artsy</h1>
+          </section>
+
       </div>
     );
   }
