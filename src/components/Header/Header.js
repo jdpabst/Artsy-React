d12ba3555d078b61d1b46ff837becd445204670c
Jessica <jdhpabst@gmail.com>
2017-08-08 15:27:08 -0600

    working on hamburger animations

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,12 +4,16 @@ import './Header.css';
 
 
 class Header extends Component {
+    constructor(props){
+        super(props);
+        
+    }
 
   render() {
     return (
       <div className="header">
           <section id="mobile_header">
-              <div className='ham'>
+              <div className='ham' >
                   <div id="bar1"></div>
                   <div id="bar2"></div>
                   <div id="bar3"></div>
