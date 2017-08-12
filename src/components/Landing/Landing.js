9f44331472918a8ef4d34d725021f5a7b280b53a
Jessica <jdhpabst@gmail.com>
2017-08-12 16:08:11 -0600

    working on second section

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -9,6 +9,7 @@ class Landing extends Component {
       <div className="landing">
           <div id="splash1"></div>
           <div id="splash2"></div>
+          <div id="splash3"></div>
           <h3 id="about_paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h3>
       </div>
     );
