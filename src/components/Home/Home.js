60e33279962f89f53e3f114252f070eb5657cd18
Jessica <jdhpabst@gmail.com>
2017-08-12 16:03:34 -0600

    working on second section

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -11,7 +11,7 @@ class Home extends Component {
     return (
       <div className="home">
           <Header/>
-          {/* <Landing/> */}
+          <Landing/> 
           {/* { router } */}
       </div>
     );
--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -7,7 +7,9 @@ class Landing extends Component {
   render() {
     return (
       <div className="landing">
-          {/* <h3>Welcome!</h3> */}
+          <div id="splash1"></div>
+          <div id="splash2"></div>
+          <h3 id="about_paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h3>
       </div>
     );
   }
