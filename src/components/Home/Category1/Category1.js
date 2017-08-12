bf02fa5163fa8012e3ccdb5951769a6e2731b736
Jessica <jdhpabst@gmail.com>
2017-08-12 16:22:28 -0600

    added category 1 component

---
+++ src/components/Home/Category1/Category1.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './Flowers.css';
+
+
+class Flowers extends Component {
+
+  render() {
+    return (
+      <div className="flowers">
+          <div id="splash1"></div>
+          <h3 id="about_paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h3>
+      </div>
+    );
+  }
+}
+
+
+export default Flowers;
