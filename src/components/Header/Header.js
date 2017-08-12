b13266aa6fecd2832af96b817e7ddc2127c81b49
Jessica <jdhpabst@gmail.com>
2017-08-12 11:25:52 -0600

    updating mobile menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -40,6 +40,15 @@ class Header extends Component {
                     <div>HOME</div>
                     <div>SHOP</div>
                     <div>CART</div>
+                    <div>CONTACT</div>
+                    <div>ABOUT US</div>
+                    <div>CONNECT</div>
+                    <section id='socials'>
+                        <img src="https://static1.squarespace.com/static/54c1c3d5e4b0707229678eed/54d57a95e4b0b8799a5a38b7/54d57a95e4b0b8799a5a38b8/1423276921378/iconmonstr-facebook-4-icon.png"/>
+                        <img src="http://static1.squarespace.com/static/51685161e4b0e454d76e8167/55f0ab3ee4b029a70084f3dd/55f0ae7de4b09c801f7cd30e/1441836746255/pinterest.png"/>
+                        <img src="https://www.cycleto.ca/sites/default/files/twitter-icon-circle-logo.png"/>
+                        <img src="https://s3.amazonaws.com/freebiesupply/large/2x/instagram-icon-white-on-black-circle.png"/>
+                    </section>
                 </section>
                 <h1 className="header_logo">Artsy</h1>
                 <div id="water_color_bg"></div>
