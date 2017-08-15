4da75fa0c90451b8c8d81df86b0d600c5d1fc76c
Jessica <jdhpabst@gmail.com>
2017-08-15 14:32:42 -0600

    created Shop component

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Shop.css';
+
+
+class Shop extends Component {
+
+  render() {
+    return (
+      <div className="shop">
+          Shop Area
+      </div>
+    );
+  }
+}
+
+
+export default Shop;
--- src/router.js
+++ src/router.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Home from './components/Home/Home.js';
+import Shop from './components/Shop/Shop.js';
 
 
 export default (
     <Switch>
         
         <Route component={ Home } path='/' exact />
+        <Route component={ shop } path='/shop' />
 
     </Switch>
 )
