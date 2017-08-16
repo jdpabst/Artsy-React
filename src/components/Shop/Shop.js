b28245da12a8ba8fcd5fa51c3c2e6ac4d7a53b53
Jessica <jdhpabst@gmail.com>
2017-08-15 20:35:59 -0600

    added menu functionality to mobile view

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -6,6 +6,27 @@ import './Shop.css';
 
 
 class Shop extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            drop: false
+        }
+        this.handleDropDown = this.handleDropDown.bind(this);
+    }
+
+    handleDropDown(){
+        if(!this.state.drop){
+            document.getElementById('mobile_menu').style.display = 'block';
+            this.setState({
+                drop: true,
+            })
+        } else{
+            document.getElementById('mobile_menu').style.display = 'none';
+            this.setState({
+                drop: false,
+            })
+        }
+    }
 
   render() {
     let settings = {
