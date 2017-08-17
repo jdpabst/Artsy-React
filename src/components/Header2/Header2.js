6a472068381efd96b29ed19f1d56135efc002c6b
Jessica <jdhpabst@gmail.com>
2017-08-17 15:18:55 -0600

    working on header2 styling issues

--- src/components/Header2/Header2.js
+++ src/components/Header2/Header2.js
@@ -28,8 +28,9 @@ class Header2 extends Component {
   render() {
     return (
         <div className="header2">
-            <h1 className="header_logo">Artsy</h1>
+            
             <section id="mobile_header2">
+                <h1 className="header_logo2">Artsy</h1>
                 <div className='ham'  onClick={ this.handleDropDown }>
                   <div id="bar1"></div>
                   <div id="bar2"></div>
