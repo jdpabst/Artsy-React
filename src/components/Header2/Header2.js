74f55dd516d327936cf33a6c19fd8ad0d39cf98c
Jessica <jdhpabst@gmail.com>
2017-08-17 15:10:27 -0600

    added a Header2 to simplify the header issues I am having

---
+++ src/components/Header2/Header2.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './Header2.css';
+
+
+class Header2 extends Component {
+    
+  render() {
+    return (
+      <div className="Header2">
+        
+        </div>
+    );
+  }
+}
+
+
+export default Header2;
